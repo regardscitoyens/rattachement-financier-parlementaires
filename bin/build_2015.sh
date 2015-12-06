ca3304f4e590fdbe919ccc98ec2527cbedeecbc8
RouxRC <b.ooghe@gmail.com>
2015-12-07 00:18:08 +0100

    add pdf an 2016

--- bin/build_2015.sh
+++ bin/build_2015.sh
@@ -4,12 +4,13 @@ CACHE=$1
 if [ -z "$CACHE" ]; then
   wget -q "http://www.nosdeputes.fr/deputes/json" -O cache/deputes.json
   wget -q "http://www.nossenateurs.fr/senateurs/json" -O cache/senateurs.json
-  wget -q "http://www.assemblee-nationale.fr/qui/Rattachement_partis_2015.csv" -O pdfs/1412-AN-rattachement-2015.csv
+  wget -q "http://www.assemblee-nationale.fr/qui/Rattachement_partis_2015.pdf" -O pdfs/1412-AN-rattachement-2015.pdf
+
   wget -q "http://www.assemblee-nationale.fr/14/tribun/xml/liste_rattachement_partis_2015.csv" -O pdfs/1412-AN-rattachement-2015.csv
   wget -q "http://www.senat.fr/fileadmin/Fichiers/Images/role/declarations_rattachement_parti_politique.pdf" -O pdfs/1412-Sénat-rattachement-2015.pdf
 fi
 
-# TODO AN
+# Complete CSV AN
 bin/complete_an.py pdfs/1412-AN-rattachement-2015.csv > data/1412-AN-rattachement-2015.csv
 
 # PDF Sénat
--- bin/build_2016.sh
+++ bin/build_2016.sh
@@ -4,13 +4,13 @@ CACHE=$1
 if [ -z "$CACHE" ]; then
   wget -q "http://www.nosdeputes.fr/deputes/json" -O cache/deputes.json
   wget -q "http://www.nossenateurs.fr/senateurs/json" -O cache/senateurs.json
+  wget -q "http://www.assemblee-nationale.fr/qui/Rattachement_partis_2016.pdf" -O pdfs/1512-AN-rattachement-2016.pdf
   echo "Nom;Prénom;Groupe;Parti ou groupement politique" | iconv -t ISO88591 > pdfs/1512-AN-rattachement-2016.csv
   wget -q "http://www.assemblee-nationale.fr/14/tribun/xml/liste_rattachement_partis_2016.csv" -O - | sed 's/\r//' >> pdfs/1512-AN-rattachement-2016.csv
-  wget -q http://www.assemblee-nationale.fr/qui/Rattachement_partis_2016.pdf -O pdfs/1512-AN-rattachement-2016.pdf
 #  wget -q "http://www.senat.fr/fileadmin/Fichiers/Images/role/declarations_rattachement_parti_politique.pdf" -O pdfs/1512-Sénat-rattachement-2016.pdf
 fi
 
-# TODO AN
+# Complete CSV AN
 bin/complete_an.py pdfs/1512-AN-rattachement-2016.csv > data/1512-AN-rattachement-2016.csv
 
 # PDF Sénat
